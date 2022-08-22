fa95a2ffd740aab22f08fb75d2a47086208eef53
Jakavickis <vytautasjkvcks@gmail.com>
2022-08-22 21:16:07 +0300

    5 numbers random to array, weigth +1

--- 001/app.js
+++ 001/app.js
@@ -1,13 +1,22 @@
-const array = []
 
+const cats = [
+    { name: 'Pilkis', weight: 4 },
+    { name: 'Murka', weight: 7 },
+    { name: 'Keris', weight: 3 }
+];
 function rand(min, max) {
-    for (i = 0; i < 10; i++) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        array.push(Math.floor(Math.random() * (max - min + 1) + min));
-    }
-}
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const mas = [...Array(5)].map(_ => rand(100, 999));
+
+console.log(mas);
+
 
 rand(100, 999)
 
-console.log(array)
+
+const weight = [...cats].map(mass => mass.weight + 1)
+console.log(weight)

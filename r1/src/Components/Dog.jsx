8c8d6ac051a6a954bad232715a68d2b69516f93b
Jakavickis <vytautasjkvcks@gmail.com>
2022-08-24 23:12:34 +0300

    react random three colors

--- r1/src/Components/Dog.jsx
+++ r1/src/Components/Dog.jsx
@@ -5,7 +5,7 @@ function Dog() {
     return (
         <>
             <h2 style={{
-                color: rand(0, 1) ? 'red' : 'blue',
+                color: ['green', 'red', 'blue'][rand(0, 2)],
                 fontSize: '50px'
             }}>Cat {false ? 'Jo' : 'GoGo'}</h2>
             <h3 className="dog">Big {rand(100, 999)}</h3>

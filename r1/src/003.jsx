dc53ea338e4e8cbf9f6aca460ff46fe62247177a
Jakavickis <vytautasjkvcks@gmail.com>
2022-08-25 19:27:06 +0300

    react sort li inner html

---
+++ r1/src/003.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+// import Dog from './Components/Dog';
+import Cat from './Components/Cat';
+import Racoon from './Components/Racoon';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <Dog></Dog> */}
+        <h1>My love Denisa</h1>
+        {/* <Dog></Dog> */}
+        <Cat></Cat>
+        <Racoon></Racoon>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,20 +1,27 @@
 import './App.css';
-// import Dog from './Components/Dog';
-import Cat from './Components/Cat';
-import Racoon from './Components/Racoon';
+
+const cats = [
+    { id: 1, weight: 5, name: 'Murka' },
+    { id: 2, weight: 8, name: 'Pilkis' },
+    { id: 3, weight: 2, name: 'Pūkis' },
+    { id: 4, weight: 3, name: 'Rainis' },
+    { id: 5, weight: 5, name: 'Brisius' }
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <Dog></Dog> */}
-        <h1>My love Denisa</h1>
-        {/* <Dog></Dog> */}
-        <Cat></Cat>
-        <Racoon></Racoon>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Lists</h1>
+                <ul>
+                    {
+                        // cats.map(cat => <li key={cat.id}>{cat.name}</li>)
+                        [...cats].sort((a, b) => b.weight - a.weight).map((a, i) => <li key={i}>{a.name} <i>{a.weight}kg</i></li>)
+                    }
+                </ul>
+            </header>
+        </div>
+    );
 }
 
 export default App;

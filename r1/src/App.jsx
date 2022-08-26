a47667ad17c8a1f93b7417d417dcc95659248fca
Jakavickis <vytautasjkvcks@gmail.com>
2022-08-26 18:50:44 +0300

    button click react

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,10 +1,20 @@
 import './App.scss';
 
 function App() {
+    const doJob = () => {
+        console.log('do job')
+    }
+
+    const clicked = () => {
+        console.log('Hellow world');
+        doJob();
+    }
+
     return (
         <div className="App">
             <header className="App-header">
                 <h1>STATE</h1>
+                <button onClick={() => clicked()}>click!</button>
             </header>
         </div>
     );

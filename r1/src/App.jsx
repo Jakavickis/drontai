d8e3d3708915416be59dcb8c73aa466415c6f6ac
Jakavickis <vytautasjkvcks@gmail.com>
2022-08-28 23:09:42 +0300

    setSize with state react

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,20 +1,32 @@
+import { useState } from 'react';
 import './App.scss';
 
 function App() {
-    const doJob = () => {
-        console.log('do job')
-    }
+
+    const [color, setColor] = useState('crimson');
+    const [size, setSize] = useState(30);
+
+    // const doJob = () => {
+    //     console.log('do job');
+    // }
 
     const clicked = () => {
-        console.log('Hellow world');
-        doJob();
+        setColor('skyblue')
+    }
+
+    const doSize = () => {
+        setSize('40')
     }
 
     return (
         <div className="App">
             <header className="App-header">
-                <h1>STATE</h1>
+                <h1 style={{
+                    color: color,
+                    fontSize: size + 'px'
+                }}>STATE</h1>
                 <button onClick={() => clicked()}>click!</button>
+                <button onClick={() => doSize()}>font size!</button>
             </header>
         </div>
     );

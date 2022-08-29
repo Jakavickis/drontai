1bcf8db85f65bb6637e94e3dec72c15bc5329d4b
Jakavickis <vytautasjkvcks@gmail.com>
2022-08-29 19:05:15 +0300

    fix folders, adding squares state

---
+++ r1/src/Components/006/Squares.jsx
@@ -0,0 +1,26 @@
+
+import { useState } from "react";
+
+function Squares() {
+
+    const [sq, setSq] = useState([]);
+
+
+    const add = () => {
+        setSq(s => [...s, '']);
+    }
+
+    return (
+        <>
+            <div className="container">
+                {
+                    sq.map((_, i) => <div key={i}></div>)
+                }
+            </div>
+            <button onClick={add}>add square</button>
+        </>
+    )
+
+}
+
+export default Squares;

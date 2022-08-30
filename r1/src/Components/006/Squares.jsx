a5fa92c96614578b3c12e3d562a9194f5ac72de8
Jakavickis <vytautasjkvcks@gmail.com>
2022-08-30 19:53:31 +0300

    practicing react sort, filter

--- r1/src/Components/006/Squares.jsx
+++ r1/src/Components/006/Squares.jsx
@@ -8,11 +8,30 @@ function Squares() {
     const [sq, setSq] = useState([]);
 
     const add = () => {
-        setSq(s => [...s, { number: rand(100, 999), color: randColor() }]);
+        // setSq(s => [...s, '']) tiesiog kvadratukas
+        setSq(s => [...s, {
+            number: rand(100, 999),
+            color: randColor(),
+            row: s.length,
+            show: true
+        }]);
     }
 
-    const sortNumbers = () => {
+    const sortHigh = () => {
         setSq(s => [...s].sort((a, b) => b.number - a.number))
+    }
+    const sortLow = () => {
+        setSq(s => [...s].sort((a, b) => a.number - b.number))
+    }
+    const noSort = () => {
+        setSq(s => s.map(square => ({ ...square, show: true }))
+            .sort((a, b) => a.row - b.row))
+    }
+    const filterBlack = () => {
+        setSq(s => s.map(square => square.number < 300 ? { ...square, show: true } : { ...square, show: false }))
+    }
+    const filterColored = () => {
+        setSq(s => s.map(square => square.number < 300 ? { ...square, show: false } : { ...square, show: true }))
     }
 
     return (
@@ -20,15 +39,23 @@ function Squares() {
             <h1>STATE {sq.filter(s => s.number < 300).length}</h1>
             <div className="container">
                 {
-                    sq.map((e, i) => <div style={{
+                    sq.map((e, i) => e.show ? <div style={{
                         fontSize: 25 + 'px',
                         backgroundColor: e.number > 300 ? e.color : 'black',
                         borderRadius: e.number % 2 ? 50 + '%' : null,
-                    }} key={i}>{e.number}</div>)
+                    }} key={i}>{e.number}</div> : null)
                 }
             </div>
             <button onClick={add}>add square</button>
-            <button onClick={sortNumbers}>sort</button>
+            <div className="container">
+                <button onClick={sortHigh}>sort high low</button>
+                <button onClick={sortLow}>sort low hight</button>
+            </div>
+            <button onClick={noSort}>square no sort</button>
+            <div className="container">
+                <button onClick={filterBlack}>only black</button>
+                <button onClick={filterColored}>all collored</button>
+            </div>
         </>
     )
 }

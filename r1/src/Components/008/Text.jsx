9f5931d29794a078aa72b969104f9beaa552d46c
Jakavickis <vytautasjkvcks@gmail.com>
2022-08-31 23:10:59 +0300

    setColor on lick

--- r1/src/Components/008/Text.jsx
+++ r1/src/Components/008/Text.jsx
@@ -1,30 +1,40 @@
 import { useState } from 'react';
 
+
 function Text() {
 
     const [text, setText] = useState('');
-    const [color, setColor] = useState('crimson');
-
+    const [color, setColor] = useState('#7caa0c');
+    const [range, setRange] = useState(0);
 
     const control = e => {
-        setText(e.target.value)
+        // Number(e.target.value) && setText(e.target.value);
+        setText(e.target.value);
     }
-    const changeColor = e => {
-        setColor(e.target.value)
+    const colorControl = e => {
+        setColor(e.target.value);
+    }
+    const rangeControl = e => {
+        setRange(e.target.value);
     }
 
     return (
 
         <>
             <div className="form-container">
-                <h2 style={{
-                    color: color
-                }}>TEXT</h2>
+                <h2>TEXT</h2>
                 <input type="text" onChange={control} value={text}></input>
-                <h1 style={{
-                    color: color
-                }}>COLOR</h1>
-                <input type="color" onChange={changeColor}></input>
+            </div>
+            <div className="form-container">
+                <h2 style={{
+                    color
+                }}>COLOR</h2>
+                <input type="color" onChange={colorControl} value={color}></input>
+                <button onClick={() => setColor('red')}>color red</button>
+            </div>
+            <div className="form-container">
+                <h2>RANGE: {range}</h2>
+                <input type="range" onChange={rangeControl} value={range}></input>
             </div>
         </>
     )

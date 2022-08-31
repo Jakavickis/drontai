364cf372e5b7301a986285b6d8e5873e97ba0cfa
Jakavickis <vytautasjkvcks@gmail.com>
2022-08-31 23:40:08 +0300

    buttong setRange plus 10

--- r1/src/Components/008/Text.jsx
+++ r1/src/Components/008/Text.jsx
@@ -16,7 +16,7 @@ function Text() {
         setColor(e.target.value);
     }
     const rangeControl = e => {
-        setRange(e.target.value);
+        setRange(parseInt(e.target.value));
     }
 
     return (
@@ -35,8 +35,13 @@ function Text() {
                 <button onClick={() => setColor('red')}>color red</button>
             </div>
             <div className="form-container">
-                <h2>RANGE: {range}</h2>
+                <h2>RANGE: {('' + range)}</h2>
                 <input type="range" onChange={rangeControl} value={range}></input>
+
+                <div className="container div">
+                    <button onClick={() => setRange(s => s - 10)}>minus 10</button>
+                    <button onClick={() => setRange(s => s + 10)}>plus 10</button>
+                </div>
             </div>
         </>
     )

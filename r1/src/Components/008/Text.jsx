d4113c1fd342368f843d9061dd2db69046c4f348
Jakavickis <vytautasjkvcks@gmail.com>
2022-08-31 20:11:50 +0300

    change color input

--- r1/src/Components/008/Text.jsx
+++ r1/src/Components/008/Text.jsx
@@ -3,18 +3,30 @@ import { useState } from 'react';
 function Text() {
 
     const [text, setText] = useState('');
+    const [color, setColor] = useState('crimson');
+
 
     const control = e => {
-        setText(Number(e.target.value))
+        setText(e.target.value)
+    }
+    const changeColor = e => {
+        setColor(e.target.value)
     }
 
     return (
 
-
-        <div className="form-container">
-            <h2>TEXT</h2>
-            <input type="text" onChange={control} value={text}></input>
-        </div>
+        <>
+            <div className="form-container">
+                <h2 style={{
+                    color: color
+                }}>TEXT</h2>
+                <input type="text" onChange={control} value={text}></input>
+                <h1 style={{
+                    color: color
+                }}>COLOR</h1>
+                <input type="color" onChange={changeColor}></input>
+            </div>
+        </>
     )
 }
 

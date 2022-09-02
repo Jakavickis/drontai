5a31ef72435814cec75c63252e03220f4b307cb7
Jakavickis <vytautasjkvcks@gmail.com>
2022-09-02 18:54:36 +0300

    State uplifting

---
+++ r1/src/008.jsx
@@ -0,0 +1,22 @@
+import './App.scss';
+// import Text from './Components/008/Text';
+// import Select from './Components/008/Select';
+import Checkbox from './Components/008/Checkbox';
+// import Sq from './Components/008/Sq';
+
+function App() {
+
+
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                {/* <Text /> */}
+                {/* <Select /> */}
+                <Checkbox />
+            </header>
+        </div>
+    );
+}
+
+export default App;
--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,19 +1,18 @@
+
 import './App.scss';
-// import Text from './Components/008/Text';
-// import Select from './Components/008/Select';
-import Checkbox from './Components/008/Checkbox';
-// import Sq from './Components/008/Sq';
+import Text from './Components/010/Text';
+import { useState } from 'react';
 
 function App() {
 
-
+    const [textNow, setTextNow] = useState('');
 
     return (
         <div className="App">
             <header className="App-header">
-                {/* <Text /> */}
-                {/* <Select /> */}
-                <Checkbox />
+                <h1>State Uplifting</h1>
+                <h2>TEXT: {textNow}</h2>
+                <Text setTextNow={setTextNow} />
             </header>
         </div>
     );

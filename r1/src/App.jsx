6e0ec08040e2e11fd9060ee2f1ff5c789fd47c0e
Jakavickis <vytautasjkvcks@gmail.com>
2022-09-06 22:30:57 +0300

    localStorage update

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -6,21 +6,23 @@ import './App.scss';
 
 function App() {
 
-    const [counts, setCounts] = useState({ one: 0, two: 0 });
+    const [counts, setCounts] = useState(null);
 
     useEffect(() => {
 
         let data = localStorage.getItem('counts_key')
         if (null === data) {
-            data = { one: 0, two: 0 };
+            setCounts({ one: 0, two: 0 });
+        } else {
+            setCounts(JSON.parse(data))
         }
-
-        setCounts(JSON.parse(data))
 
     }, [])
 
     useEffect(() => {
-
+        if (null === counts) {
+            return;
+        }
         localStorage.setItem('counts_key', JSON.stringify(counts))
 
     }, [counts])
@@ -29,8 +31,8 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <h2>local storage</h2>
-                <h2>ONE: {counts.one}</h2>
-                <h2>Two: {counts.two}</h2>
+                <h2>ONE: {counts?.one}</h2>
+                <h2>Two: {counts?.two}</h2>
                 <button onClick={() => setCounts(c => ({ ...c, one: c.one + 1 }))}>one +</button>
                 <button onClick={() => setCounts(c => ({ ...c, two: c.two + 1 }))}>two +</button>
             </header>

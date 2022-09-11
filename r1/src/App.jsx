bfe97c5846f16952d80036cafa4c4429cfdfb9bf
Jakavickis <vytautasjkvcks@gmail.com>
2022-09-11 19:06:59 +0300

    balls

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -5,6 +5,7 @@ import CbcChange from './Components/014/CbcChange';
 import ManyButtons from './Components/014/ManyButtons';
 import Picture from './Components/014/Picture';
 import RandNumbers from './Components/014/RandNumbers';
+import Sircle from './Components/014/Sircles';
 import T3 from './Components/014/T3';
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
                 <T3 />
                 <Picture />
                 <ManyButtons />
+                <Sircle />
             </header>
 
         </div >

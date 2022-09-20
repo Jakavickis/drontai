2e9670b2a0dc9edf0affeaa8c4bc07bbc66f2953
Jakavickis <vytautasjkvcks@gmail.com>
2022-09-20 22:06:14 +0300

    counter3

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -6,6 +6,7 @@ function App() {
 
     const [counter, setCounter] = useState(0);
     const [counter2, setCounter2] = useState(0);
+    const [counter3, setCounter3] = useState(0);
 
     useEffect(() => {
         const intervalId = setInterval(() => {
@@ -25,12 +26,25 @@ function App() {
         }
     }, []);
 
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCounter3(c => c >= 5 ? 0 : c + 1);
+        }, 1000);
+        return () => {
+            clearInterval(intervalId);
+        }
+    }, []);
 
     return (
         <div className="App">
             <header className="App-header">
                 <h2>{counter}</h2>
                 <h2>{counter2}</h2>
+                <div className="container">
+                    {
+                        [...Array(counter3)].map((_, i) => <div key={i}></div>)
+                    }
+                </div>
             </header>
         </div>
     );

6aeec8c479e6dfb702e5462ae104d15d36663d9f
Jakavickis <vytautasjkvcks@gmail.com>
2022-09-20 21:58:55 +0300

    counter2

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -5,6 +5,7 @@ import './App.scss';
 function App() {
 
     const [counter, setCounter] = useState(0);
+    const [counter2, setCounter2] = useState(0);
 
     useEffect(() => {
         const intervalId = setInterval(() => {
@@ -15,13 +16,21 @@ function App() {
         }
     }, []);
 
-
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCounter2(c => c >= 10 ? 0 : c + 2);
+        }, 1000);
+        return () => {
+            clearInterval(intervalId);
+        }
+    }, []);
 
 
     return (
         <div className="App">
             <header className="App-header">
                 <h2>{counter}</h2>
+                <h2>{counter2}</h2>
             </header>
         </div>
     );

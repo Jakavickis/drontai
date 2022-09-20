fe070bb50c10b4e66501dc1b4c3010e3207ae884
Jakavickis <vytautasjkvcks@gmail.com>
2022-09-20 21:54:51 +0300

    setInterval

---
+++ r1/src/019.jsx
@@ -0,0 +1,18 @@
+import './App.scss';
+import Books from './Components/019/Books';
+
+function App() {
+
+
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h2>CLEAN ME FOR SMOOTH LOOK, FIX ME PLS BIETER XD</h2>
+                <Books />
+            </header>
+        </div >
+    );
+}
+
+export default App;
--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,17 +1,29 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
 import './App.scss';
-import Books from './Components/019/Books';
 
 function App() {
+
+    const [counter, setCounter] = useState(0);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCounter(c => c + 1);
+        }, 1000);
+        return () => {
+            clearInterval(intervalId);
+        }
+    }, []);
+
 
 
 
     return (
         <div className="App">
             <header className="App-header">
-                <h2></h2>
-                <Books />
+                <h2>{counter}</h2>
             </header>
-        </div >
+        </div>
     );
 }
 
